b00f72949fe8d2b483e80b5226da8fa5a0e192b6
Rayyan Haroon <000427071@student.vcc.ca>
2020-12-17 19:20:51 -0800

    pushing project?

---
+++ Stack/Stack/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        public Stack(int size)
+        {
+
+        }
+        public void Push(T item)
+        {
+
+        }
+        public T Pop()
+        {
+            T t = default(T);
+            return t;
+        }
+        public T Peek()
+        {
+            T t = default(T);
+            return t;
+        }
+        public int Count()
+        {
+            return 0;
+        }
+    }
+}
