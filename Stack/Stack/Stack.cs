ecf4f546e6b13ecb00b01dc0458ed98e07bd1931
Rayyan Haroon <000427071@student.vcc.ca>
2020-12-17 20:28:41 -0800

    trying to implement but not going well

--- Stack/Stack/Stack.cs
+++ Stack/Stack/Stack.cs
@@ -10,21 +10,29 @@ namespace Stack
     {
         public Stack(int size)
         {
-
+            
         }
         public void Push(T item)
         {
-
+            item.Push(1);
+            item.Push(2);
+            item.Push(3);
         }
         public T Pop()
         {
             T t = default(T);
+            int pop = Stack.Pop();
             return t;
         }
         public T Peek()
         {
             T t = default(T);
-            return t;
+            if (T.Count > 0)
+            {
+                Console.WriteLine(t.Peek()); // prints 4
+                Console.WriteLine(t.Peek()); // prints 4
+                return t;
+            }
         }
         public int Count()
         {
